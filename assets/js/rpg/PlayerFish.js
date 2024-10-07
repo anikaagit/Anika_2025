9270080280b177c9704193a1966ee27ce9150eab
anikaagit <anika.marathe@gmail.com>
2024-10-06 23:35:34 -0700

    Adding hack so that turle starts at center right and fish starts at center left

---
+++ assets/js/rpg/PlayerFish.js
@@ -0,0 +1,66 @@
+import Player from './Player.js';
+import GameEnv from './GameEnv.js'; //added this line to reference in position below
+
+
+class Fish extends Player {
+    constructor(imageSrc = null) {
+        super(imageSrc,{ x: 400, y: 290 });
+        // initiialize fish to Center/Left position
+        this.position = { x: 0, y: (GameEnv.innerHeight - this.size) / 2 }; 
+    }
+
+    update() {
+        super.update();
+    }
+
+    resize() {
+        super.resize();
+    }
+
+    handleKeyDown({ keyCode }) {
+        switch (keyCode) {
+            case 87: // 'W' key
+                this.velocity.y -= this.yVelocity;
+                this.direction = 'up';
+                break;
+            case 65: // 'A' key
+                this.velocity.x -= this.xVelocity;
+                this.direction = 'left';
+                break;
+            case 83: // 'S' key
+                this.velocity.y += this.yVelocity;
+                this.direction = 'down';
+                break;
+            case 68: // 'D' key
+                this.velocity.x += this.xVelocity;
+                this.direction = 'right';
+                break;
+        }
+    }
+
+    /**
+     * Handles key up events to stop the player's velocity.
+     * 
+     * This method stops the player's velocity based on the key released.
+     * 
+     * @param {Object} event - The keyup event object.
+     */
+    handleKeyUp({ keyCode }) {
+        switch (keyCode) {
+            case 87: // 'W' key
+                this.velocity.y = 0;
+                break;
+            case 65: // 'A' key
+                this.velocity.x = 0;
+                break;
+            case 83: // 'S' key
+                this.velocity.y = 0;
+                break;
+            case 68: // 'D' key
+                this.velocity.x = 0;
+                break;
+        }
+    }
+}
+
+export default Fish;
